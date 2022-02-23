1574614105da1f7cb8317cc8fe450a8c0e7861db
Santos <sg21894@gmail.com>
2022-02-22 14:04:29 -1000

    randomgame page working, cover art no longer a thumbnail

--- week7/day1/vgProject/random.js
+++ week7/day1/vgProject/random.js
@@ -1,8 +1,13 @@
 const client_id = "0fthim31xfwt3jihnhzg88nkje54t4";
 const client_secret = "6ru89t3ws8bhni4ikbr1w12md7opqc";
-const button = document.querySelector(".button");
-const input = document.querySelector("input");
-const mainBody = document.querySelector(".woo");
+const randomDiv = document.querySelector(".randomDiv");
+
+let randomArray = [];
+const getRandomNumbers = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const randomNumber = getRandomNumbers(1, 25000);
+console.log(randomNumber);
 
 //gets token
 const getToken = async () => {
@@ -21,15 +26,13 @@ const getToken = async () => {
   console.log(data);
   return data.access_token;
 };
-
-//gets game information
-const getGames = async () => {
+const getRandomGame = async () => {
   const token = await getToken();
   const result = await fetch(
     "https://corsanywhere.herokuapp.com/https://api.igdb.com/v4/games",
     {
       method: "POST",
-      body: `fields *; search "${input.value}";  limit 10;`,
+      body: `fields *; where id= ${randomNumber};  limit 10;`,
       headers: { "Client-ID": client_id, Authorization: "Bearer " + token },
       data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
     }
@@ -41,20 +44,22 @@ const getGames = async () => {
   let coverId = [];
   for (const info of data) {
     coverId.push(info.cover);
-    const gameName = document.createElement("p");
+    const gameName = document.createElement("h1");
+    const gameSummary = document.createElement("p");
     const gameCover = document.createElement("div");
     gameCover.className = info.cover;
     gameCover.setAttribute("name", `${info.cover}`);
     gameCover.setAttribute("id", `${info.cover}`);
+    gameSummary.innerText = info.summary;
 
     gameName.innerText = info.name;
-    mainBody.append(gameName);
-    mainBody.append(gameCover);
+    randomDiv.append(gameName);
+    randomDiv.append(gameCover);
+    randomDiv.append(gameSummary);
   }
   console.log(coverId);
   await getCover(coverId);
 };
-
 const getCover = async (coverId) => {
   let query = "";
   for (const iterator of coverId) {
@@ -82,11 +87,12 @@ const getCover = async (coverId) => {
     const forCover = document.getElementById(forId);
     console.log(forCover);
     const gameImage = document.createElement("img");
-    gameImage.src = cover.url;
+    // gameImage.src = cover.url;
+    console.log(cover.url);
+    let text = cover.url;
+    let replace = text.replace("thumb", "cover_big");
+    gameImage.src = replace;
     forCover.append(gameImage);
   }
 };
-button.addEventListener("click", () => {
-  getGames();
-  mainBody.innerHTML = "";
-});
+getRandomGame();
